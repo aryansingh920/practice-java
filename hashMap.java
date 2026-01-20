569ec2f11118fc5ccbe441ffe098494e98164cbf
Aryan Singh <aryansingh920@outlook.com>
2026-01-20 22:52:29 +0000

    Implement hashMap class with firstUniqueCharIndex method

---
+++ hashMap.java
@@ -0,0 +1,37 @@
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class hashMap {
+    public static int firstUniqueCharIndex(String s) {
+        HashMap<Character, LinkedList<Integer>> charIndexMap = new HashMap<>();
+        int firstIndex = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (charIndexMap.containsKey(currentChar)) {
+                LinkedList<Integer> currList = charIndexMap.get(currentChar);
+                currList.add(i);
+            } else {
+                LinkedList<Integer> newList = new LinkedList<>(List.of(i));
+                charIndexMap.put(currentChar, newList);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (charIndexMap.get(currentChar).size() == 1) {
+                firstIndex = charIndexMap.get(currentChar).get(0);
+                return firstIndex;
+            }
+        }
+        return firstIndex;
+    }
+
+    public static void main(String[] args) {
+        int Output = firstUniqueCharIndex("leeldezd");
+        System.out.println(Output);
+    }
+}
